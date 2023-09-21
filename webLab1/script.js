e77d81bd79f10e1ab01978acd84209edf33434cf
Ponomarenko-Alice <a.ponomaremko111@gmail.com>
2023-09-22 00:31:45 +0300

    -

--- webLab1/script.js
+++ webLab1/script.js
@@ -1,18 +1,10 @@
 const form = document.querySelector('.main_form');
-// const yInput = document.querySelector('.y');
-// const tbodyResult = document.querySelector('.output');
 
 form.addEventListener('submit', () => {
     const x = "1",
-    //  x = form.querySelector('[name="x"]').checked,
     y = form.querySelector('[name="y"]').value,
     r = form.querySelector('[name="r"]').value;
 
-    // const data = {
-    //     x: x.checked,
-    //     y: y.value,
-    //     r: r.value
-    // };
 
     fetch(`./handler.php?x=${x}&y=${y}&r=${r}`, {
         method: 'POST',
@@ -32,26 +24,3 @@ form.addEventListener('submit', () => {
             alert(`There was an error processing your request: ${error.message}`)
         })
 });
-
-// tbodyResult.innerHTML = '<th class="success">пум пум</th>';
-
-
-
-
-// $(document).ready(function() {
-//     $(".ouput").prepend("<tr> <td>" + newResult + "</td> </tr>");
-// });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     function validateForm() {
-//         // let x = document.getElementById("x").value;
-//         let y = document.getElementById("y").value;
-//         // let r = document.getElementById("r").value;
-
-//         if (isNaN(y)) {
-//             alert("Invalid input! Please enter valid numbers. R should be positive.");
-//             return false;
-//         }
-//         return true;
-//     }
-// });
