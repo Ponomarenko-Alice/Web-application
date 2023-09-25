898f2c15f909c4e17e77bf283ece1cc13edd0e90
Ponomarenko-Alice <a.ponomaremko111@gmail.com>
2023-09-25 23:59:18 +0300

    Refactoring -files moved to folders -renamed some files

---
+++ webLab1/js/validator.js
@@ -0,0 +1,59 @@
+export class Validator {
+    xEntity;
+    yEntity;
+    rEntity;
+    message = "";
+
+    /**
+     * 
+     * @param {*} x array of values of checkbox input
+     * @param {*} y text input
+     * @param {*} r radio input
+     */
+    constructor(x, y, r) {
+        this.xEntity = x;
+        this.yEntity = y;
+        this.rEntity = r;
+    }
+ 
+    validate() {
+        return this.checkX() && this.checkY() && this.checkR();
+    }
+
+    checkX() {
+        if (this.xEntity.length > 1) {
+            this.message = "Enter only one value of X coordinate";
+            return false;
+        } else if (this.xEntity.value = ""){
+            this.message = "Enter X value. It can't be empty";
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    checkY() {
+        let currentY = this.yEntity.value;
+        if (isNaN(currentY)) {
+            this.message = "Y value must be a number";
+            return false;
+        } else if (currentY === ""){
+            this.message = "Enter Y value. It can't be empty";
+            return false;
+        } else if (!['-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3', '4', '5'].includes(currentY)) {
+            this.message = "Y value must be from -5 to 5";
+            return false;
+        } else { 
+            return true;
+        }
+    }
+
+    checkR() {
+        if (this.rEntity.value === "") {
+            this.message = "Enter R value. It can't be empty";
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ webLab1/php/handler.php
@@ -0,0 +1,67 @@
+<?php
+
+require __DIR__ . "/coordinatesValidator.php";
+require __DIR__ . "/pointChecker.php";
+
+@session_start();
+
+if (!isset($_SESSION["results"])) {
+    $_SESSION["results"] = array();
+}
+
+$x = "";
+$y = "";
+$r = "";
+if(isset($_POST["x"])){
+    $x = $_POST["x"];
+}
+if(isset($_POST["y"])){
+    $y = $_POST["y"];
+}
+if(isset($_POST["r"])){
+    $r = $_POST["r"];
+}
+
+$validator = new coordinatesValidator($x, $y, $r);
+if ($validator->checkCoordinates()) {
+        $pointIsIsArea = pointChecker::isInArea($x, $y, $r);
+        $coordinate_status = $pointIsIsArea
+        ? "<span class='yes'>yes</span>"
+        : "<span class='no'>no</span>";
+        $currentTime = date("Y-m-d H:i:s");
+
+        $newResult = array(
+            "x" => $x,
+            "y" => $y,
+            "r" => $r,
+            "coordinate_status" => $coordinate_status,
+            "currentTime" => $currentTime
+        );
+
+        array_push($_SESSION["results"], $newResult);
+
+        echo "<table id='outputTable'>
+        <tr>
+            <th>x</th>
+            <th>y</th>
+            <th>r</th>
+            <th>status</th>
+            <th>current time</th>
+        </tr>";
+
+        foreach (array_reverse($_SESSION["results"]) as $tableRow) {
+            echo "<tr>";
+            echo "<td>" . $tableRow["x"] . "</td>";
+            echo "<td>" . $tableRow["y"] . "</td>";
+            echo "<td>" . $tableRow["r"] . "</td>";
+            echo "<td>" . $tableRow["coordinate_status"] . "</td>";
+            echo "<td>" . $tableRow["currentTime"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    
+}
+else {
+    echo "validation faild $x, $y, $r";
+}
+
